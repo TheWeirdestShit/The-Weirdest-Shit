05f3d1088c015cb25a3bab5cbca1d93ecfeb4276
Kevin Bradshaw <gaeel@nanoleptic.net>
2017-02-23 23:28:01 -0600

    Add 💩  cursor

---
+++ The Weirdest Shit/Assets/SetCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCursor : MonoBehaviour {
+
+	public Texture2D cursor;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor(cursor, Vector2.one*2, CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
