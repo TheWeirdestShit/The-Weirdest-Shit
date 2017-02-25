ce19aad256a7933e6a9997ecd69ce519365603f7
Kevin Bradshaw <gaeel@nanoleptic.net>
2017-02-24 20:11:08 -0600

    OMFG THIS GUY IS SO FUCKING CREEPY

---
+++ The Weirdest Shit/Assets/Scripts/ProjectorImageOnFlipout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorImageOnFlipout : MonoBehaviour {
+
+	public Texture2D[] images;
+
+	FlipOut fo;
+	Projector proj;
+
+	// Use this for initialization
+	void Start () {
+		fo = GetComponent<FlipOut>();
+		proj = GetComponent<Projector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fo.flipOut){
+			Material mat = new Material(proj.material);
+			mat.SetTexture("_ShadowTex", images.pickRandom());
+			proj.material = mat;
+		}
+	}
+}
